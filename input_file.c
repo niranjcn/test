dc682808481aebf1ec0cf84ecaa1317857908e3a
Devathmaj A Kaliyathan <devathmaj@gmail.com>
2025-10-13 16:46:49 +0000

    TEST2

---
+++ input_file.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+// This is a test comment
+int main(){
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    printf("Sum: %d\n", sum);
+    return 0;
+}
