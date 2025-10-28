cc8bed00e445d0dbd4c5a7ca4c9fc23f57a7587b
Niranj <122597624+niranjcn@users.noreply.github.com>
2025-10-28 21:08:22 +0530

    Update backend.c

--- backend.c
+++ backend.c
@@ -1,40 +1,37 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
 
-void main(){
-    char icode[10][30], str[10], opr[20];
-    int i=0, num=0;
-    printf("Enter the intermediate code to generate backend of compiler\n");
-    i=0;
-    while(1){
-        scanf("%s",icode[i]);
-        if(strcmp(icode[i],"exit")==0) break;
-        i++;
-    }
-    num = i;
-    printf("Intermediate code is\n");
-    i=0;
+int main(){
+    
+    printf("Enter the set of intermnediate expressions: (terminated by exit) \n");
+    char expr[100][100],str[20],opr[20];
+    int i = 0;
     do{
-        strcpy(str,icode[i]);
-        switch(str[3])
-        {
+        scanf("%s",expr[i]);
+    }while(strcmp(expr[i++], "exit") != 0);
+
+    printf("The optimized expressions after common subexpression elimination are: \n");
+    i = 0;
+    do{
+        strcpy(str, expr[i]);
+        switch(str[3]){
             case '+':
                 strcpy(opr, "ADD");
                 break;
             case '-':
                 strcpy(opr, "SUB");
-                break;
+                break;  
             case '*':
                 strcpy(opr, "MUL");
                 break;
             case '/':
                 strcpy(opr, "DIV");
                 break;
-
         }
-        printf("\nMOV %c, R%d",str[2],i);
-        printf("\n%s %c, R%d",opr,str[4],i);
-        printf("\nMOV R%d, %c",i,str[0]);
-    }while(++i < num && strcmp(icode[i],"end")!=0);
+
+        printf("\n\tMOV %c, R%d", str[2], i);
+        printf("\n\t%s %c, R%d", opr, str[4], i);
+        printf("\n\tMOV R%d, %c", i,str[0]);
+    }while(strcmp(expr[++i],"exit") != 0);
+
 }
